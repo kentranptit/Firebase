0e7a0bd899cc92997a38f1d83995394afe1fd92d
Ken Tran <kentran24797@gmail.com>
2018-01-18 15:49:14 +0700

    add demo firebase project

---
+++ NewFirebaseDemo/app/src/main/java/com/ptit/pakandroid/kentran97/newfirebasedemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ptit.pakandroid.kentran97.newfirebasedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello the World!");
+    }
+}
